5484cb57eb6772219bc5f5486f13c6222781b0d3
ficopang <pangestufico@gmail.com>
2022-07-24 19:43:47 +0700

    Fix font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-// FONT
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import * as React from 'react';
+import './App.css';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
